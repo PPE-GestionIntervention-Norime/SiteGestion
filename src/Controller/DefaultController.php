4e9dae9c4bbf8cb0feb0118e31dba99414bd38a8
MarieQuesnel21 <marie.quesnel@outlook.com>
2019-11-28 15:20:03 +0100

    Add DefaultController

---
+++ src/Controller/DefaultController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class DefaultController extends AbstractController
+{
+ 
+    /**
+     * @Route("/")
+     */
+    public function index()
+    {
+        return $this->render('default/index.html.twig', [
+            'controller_name' => 'DefaultController',
+        ]);
+    }
+}
+
