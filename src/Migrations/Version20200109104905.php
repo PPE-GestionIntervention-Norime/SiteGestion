b7ae4f228f2c3fafe6be097a15fd59b6871e0a62
MarieQuesnel21 <marie.quesnel@outlook.com>
2020-01-09 12:16:22 +0100

    resolution problem

--- src/Migrations/Version20200109104905.php
+++ src/Migrations/Version20200109104905.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20191219163256 extends AbstractMigration
+final class Version20200109104905 extends AbstractMigration
 {
     public function getDescription() : string
     {
